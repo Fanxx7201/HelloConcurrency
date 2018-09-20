75b129b1ccb0425ca4af9ede6baadcec70190435
fxx <fanxx_3516@163.com>
2018-09-20 17:31:23 +0800

    first commit

---
+++ src/main/java/com/test/confxx/ConfxxApplication.java
@@ -0,0 +1,12 @@
+package com.test.confxx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfxxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfxxApplication.class, args);
+	}
+}
---
+++ src/test/java/com/test/confxx/ConfxxApplicationTests.java
@@ -0,0 +1,16 @@
+package com.test.confxx;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConfxxApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
